9f7266d81a4771176006e671286751ca62898ca7
Grayson Croom <grayson.croom@gmail.com>
2016-11-25 19:16:28 -0600

    refactored code up from beggining of project to now

--- js/master.js
+++ js/master.js
@@ -1,18 +1,34 @@
 var boxes = document.querySelectorAll("td");
 var isXTurn = true;
 
-for(var i = 0; i < boxes.length; i++){
-	boxes[i].addEventListener("click", function(){
-		if(isXTurn && (this.textContent !== "X" && this.textContent !== "O") ){
-			this.classList.add("cross");
-			this.textContent = "X";
-		}
-		else{
-			if(this.textContent !== "X" && this.textContent !== "O"){
-				this.classList.add("circle");
-				this.textContent = "O";
-			}
-		}
-		isXTurn = !(isXTurn);
-	});
+function init(){
+	addClickLiseners(boxes);
 }
+
+function addClickLiseners(arr){
+	for(var i = 0; i < arr.length; i++){
+		arr[i].addEventListener("click", function(){
+			if(isXTurn && isEmpty(this))
+				addCross(this);
+			else if(isEmpty(this))
+				addCircle(this);
+			isXTurn = !(isXTurn);
+		});
+	}
+}
+function isEmpty(box){
+	if(box.textContent !== "X" && box.textContent !== "O")
+		return true;
+	else
+		return false;
+}
+function addCross(item){
+	item.classList.add("cross");
+	item.textContent = "X";
+}
+function addCircle(item){
+	item.classList.add("circle");
+	item.textContent = "O";
+}
+
+init();
