b2a290c177ccb72d64805fda1f0b63305888cde3
Grayson Croom <grayson.croom@gmail.com>
2016-11-26 09:41:18 -0600

    added basic checking when all slots are filled on the board

--- js/master.js
+++ js/master.js
@@ -1,4 +1,5 @@
 var boxes = document.querySelectorAll("td");
+var boxtext = document.querySelectorAll("td span");
 var player = document.querySelector("#player");
 var playerTurnDisplay = document.querySelector("#playerTurnDisplay");
 var isXTurn = true;
@@ -45,9 +46,38 @@ function displayTurn(){
 		playerTurnDisplay.classList.toggle("yTurn");
 	}
 	else{
-		playerTurnDisplay.textContent = "Draw";
-		playerTurnDisplay.classList.toggle(playerTurnDisplay.classList);
-		playerTurnDisplay.classList.add("gameOver");
+		//horizontal wins
+		if(boxes[0].textContent === boxes[1].textContent && boxes[0].textContent === boxes[2].textContent){
+			alert(boxes[0].textContent + " wins horizontally");
+		}
+		else if(boxes[3].textContent === boxes[4].textContent && boxes[3].textContent === boxes[5].textContent){
+			alert(boxes[3].textContent + " wins horizontally");
+		}
+		else if(boxes[6].textContent === boxes[7].textContent && boxes[6].textContent === boxes[8].textContent){
+			alert(boxes[6].textContent + " wins horizontally");
+		}
+		//vertical wins
+		else if(boxes[0].textContent === boxes[3].textContent && boxes[0].textContent === boxes[6].textContent){
+			alert(boxes[0].textContent + " wins vertically");
+		}
+		else if(boxes[1].textContent === boxes[4].textContent && boxes[1].textContent === boxes[7].textContent){
+			alert(boxes[1].textContent + " wins vertically");
+		}
+		else if(boxes[2].textContent === boxes[5].textContent && boxes[2].textContent === boxes[8].textContent){
+			alert(boxes[2].textContent + " wins vertically");
+		}
+		//diagonal wins
+		else if(boxes[0].textContent === boxes[4].textContent && boxes[0].textContent === boxes[8].textContent){
+			alert(boxes[2].textContent + " wins diagonally");
+		}
+		else if(boxes[2].textContent === boxes[4].textContent && boxes[2].textContent === boxes[6].textContent){
+			alert(boxes[2].textContent + " wins diagonally");
+		}
+		else{
+			playerTurnDisplay.textContent = "Draw";
+			playerTurnDisplay.classList.toggle(playerTurnDisplay.classList);
+			playerTurnDisplay.classList.add("gameOver");
+		}
 	}
 }
 function checkWinner(){
